e3ffc7e8ff523d8b45fb2fb99626e5757c543e6a
khanhn <knultimate@live.com>
2014-03-31 20:03:19 -0700

    - Generated simple sinusoidal wave (need to add Sampling rate)

--- Synthesist/src/SynthesistExerciser.cpp
+++ Synthesist/src/SynthesistExerciser.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <math.h>
 using namespace std;
 
-int main()
+#define PI 3.14159
+#define TWO_PI (2 * PI)
+
+int main(int argc, char** argv)
 {
-	cout << "Hello, world!" << endl;
+	ofstream output("data.txt");
+	double cNote[8192];
+
+	for (int i = 0; i < TWO_PI; ++i)
+	{
+		cNote[i] = 2 * sin(TWO_PI * 440 * i);
+		output << i << " " << cNote[i] << endl;
+	}
+
+	output.close();
 	return 0;
 }
